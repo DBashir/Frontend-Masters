62045a49149f2b3ec65c7abef54d057882527daa
Deen Bashir <deen.bashir@gfk.com>
2022-09-21 13:02:29 +0100

    results component added

--- adopt-me/src/results.js
+++ adopt-me/src/results.js
@@ -1,7 +1,27 @@
-import Pet from './Pet.jsx';
+import Pet from "./Pet.js";
 
-const Results = ({pets}) => {
-    return (
-        
-    )
-}
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => {
+          return (
+            <Pet
+              animal={pet.animal}
+              key={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default Results;
